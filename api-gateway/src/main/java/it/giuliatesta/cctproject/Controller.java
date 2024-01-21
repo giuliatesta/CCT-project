b991411f2a6d02eaaaca46c3f3d811b03980a983
Giulia Testa <giuliatestax@gmail.com>
2024-01-21 16:47:07 +0100

    first try of calling microservice

--- api-gateway/src/main/java/it/giuliatesta/cctproject/Controller.java
+++ api-gateway/src/main/java/it/giuliatesta/cctproject/Controller.java
@@ -1,13 +1,20 @@
 package it.giuliatesta.cctproject;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/counter")
-    public int greeting(@RequestParam(value = "counter") int counter) {
-        return ++counter;
+    @GetMapping("/get")
+    public String get() {
+        try {
+            String url = "https://localhost:9080/get";
+            RestTemplate restTemplate = new RestTemplate();
+            return restTemplate.getForObject(url, String.class);
+        } catch (Exception e) {
+            return e.toString();
+        }
     }
 }
