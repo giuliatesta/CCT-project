4113d822a9eceb427e8b952bb1eb85c5d6c5720e
Giulia Testa <giuliatestax@gmail.com>
2024-01-30 15:54:36 +0100

    added microservice url to properties and dynamically loaded them in CctServlet

---
+++ api-gateway/src/main/java/it/giuliatesta/cctproject/CctConfiguration.java
@@ -0,0 +1,32 @@
+package it.giuliatesta.cctproject;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class CctConfiguration {
+    private static final Properties configs = new Properties();
+
+    private static final CctConfiguration instance = new CctConfiguration();
+
+    static {
+        instance.load();
+    }
+
+    static CctConfiguration getInstance() {
+        return instance;
+    }
+
+    private void load() {
+        try {
+            InputStream input = CctConfiguration.class.getClassLoader().getResourceAsStream("/cct-config.properties");
+            configs.load(input);
+        } catch (Exception e) {
+            System.out.println("Error while loading microservices properties");
+            e.printStackTrace();
+        }
+    }
+
+    public String get(String config) {
+        return configs.getProperty(config);
+    }
+}
--- api-gateway/src/main/java/it/giuliatesta/cctproject/CctServlet.java
+++ api-gateway/src/main/java/it/giuliatesta/cctproject/CctServlet.java
@@ -2,14 +2,15 @@ package it.giuliatesta.cctproject;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.HttpServerErrorException.NotImplemented;
 
+import jakarta.el.PropertyNotFoundException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,6 +19,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "gateway", description = "Custom API gateway")
 public class CctServlet extends HttpServlet {
+
+    static final int NUMBER_OF_SERVICES = 2;
 
     private final RestTemplate restTemplate;
 
@@ -28,27 +31,24 @@ public class CctServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String endpoint = getEndPointFromRequest(req);
-        switch (endpoint) {
-            case "json":
-                call(0, resp);
-                break;
-            case "html":
-                call(1, resp);
-            default:
-                // not handled
-                resp.setStatus(404);
+        if ("".equals(endpoint) || "/".equals(endpoint)) {
+            resp.getWriter().println("Invalid request");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
         }
-
+        call(endpoint, randomlyChooseMicroservice(), resp);
     }
 
-    private void call(int microservice, HttpServletResponse resp) throws ServletException, IOException {
+    private void call(String endpoint, String microservice, HttpServletResponse resp)
+            throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         // TODO headers
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer your_token");
         HttpEntity<String> entity = new HttpEntity<>(headers);
         try {
-            ResponseEntity<String> response = restTemplate.exchange(getUrl(microservice), HttpMethod.GET,
+            String url = composeUrl(endpoint, microservice);
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET,
                     entity, String.class);
 
             if (response.getStatusCode().is2xxSuccessful()) {
@@ -70,14 +70,27 @@ public class CctServlet extends HttpServlet {
         return splits[splits.length - 1];
     }
 
-    private String getUrl(int microservice) {
-        switch (microservice) {
-            case 0:
-                return "http://httpbin1:80/json";
-            case 1:
-                return "http://httpbin2:80/html";
-            default:
-                throw new IllegalArgumentException();
+    private String composeUrl(String endpoint, String microservice) {
+        CctConfiguration config = CctConfiguration.getInstance();
+        String microserviceUrl = config.get(microservice);
+        // if the microservice requested is not supported
+        if (microserviceUrl == null || microserviceUrl.isEmpty()) {
+            throw new PropertyNotFoundException();
+        }
+
+        if (!microserviceUrl.endsWith("/")) {
+            microserviceUrl += "/";
+        }
+
+        return microserviceUrl + endpoint;
+    }
+
+    private String randomlyChooseMicroservice() {
+        var random = new Random();
+        if (random.nextInt(NUMBER_OF_SERVICES - 1) == 1) {
+            return "httpbin1";
+        } else {
+            return "httpbin2";
         }
     }
 }
