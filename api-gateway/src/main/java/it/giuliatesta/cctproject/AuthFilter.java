9216ee75127b77aae64f3b31d77dbe22490b8257
Giulia Testa <giuliatestax@gmail.com>
2024-02-04 17:09:37 +0100

    added role check of JWT in authorization header of the request

--- api-gateway/src/main/java/it/giuliatesta/cctproject/AuthFilter.java
+++ api-gateway/src/main/java/it/giuliatesta/cctproject/AuthFilter.java
@@ -1,8 +1,6 @@
 package it.giuliatesta.cctproject;
 
 import java.io.IOException;
-import java.time.Instant;
-import java.util.Date;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
@@ -22,21 +20,65 @@ public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        var token = ((HttpServletRequest) request).getHeader("Authorization");
-        if (Auth.validate(token)) {
-            System.out.println("[Auth Filter] Request validated");
-            // Token is valid, proceed with the chain
-            chain.doFilter(request, response);
-        } else {
-            System.out.println("[Auth Filter] Request rejected");
-            // Token is invalid, return unauthorized response
-            response.getWriter().write("Invalid token");
-            ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        var httpRequest = (HttpServletRequest) request;
+        var httpResponse = (HttpServletResponse) response;
+        var validation = Auth.validate(httpRequest.getHeader("Authorization"), httpRequest.getMethod());
+        switch (validation) {
+            case INVALID_TOKEN:
+                // Token is invalid, return unauthorized response
+                System.out.println("[Auth Filter] Request rejected due to invalid token");
+                httpResponse.getWriter().write("Invalid token");
+                httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                break;
+            case INVALID_PERMISSIONS:
+                // Token doesn't have the correct permissions for this type of request.
+                System.out.println("[Auth Filter] Request rejected due to insufficient permissions");
+                httpResponse.getWriter().write("Insufficient permissions");
+                httpResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            case AUTHORIZED:
+                // Token is valid, proceed with the chain
+                System.out.println("[Auth Filter] Request validated");
+                chain.doFilter(request, response);
         }
     }
 }
 
+enum AuthResponse {
+    AUTHORIZED, INVALID_TOKEN, INVALID_PERMISSIONS
+}
+
 class Auth {
+    public static AuthResponse validate(Object authorizationToken, String method) {
+        try {
+            if (authorizationToken == null) {
+                System.out.println("[Auth] Authorization header not found. Request rejected");
+                return AuthResponse.INVALID_TOKEN;
+            }
+
+            String strToken = authorizationToken.toString();
+            if (!strToken.startsWith("bearer ")) {
+                System.out.println("[Auth] Invalid authorization header format. Request rejected");
+                return AuthResponse.INVALID_TOKEN;
+            }
+            // remove the "bearer " prefix
+            strToken = strToken.substring(7);
+
+            // HS256 (HMAC with SHA-256): same key for generating and validating.
+            Algorithm algorithm = Algorithm.HMAC256(getSecret());
+            var decoded = JWT.require(algorithm).build().verify(strToken);
+            var role = decoded.getClaim("role").asString();
+
+            // admin can do anything
+            // everybody else can only GET, HEAD and OPTIONS
+            if (!"admin".equals(role) && !allowedMethod(method)) {
+                return AuthResponse.INVALID_PERMISSIONS;
+            }
+            return AuthResponse.AUTHORIZED;
+        } catch (Exception e) {
+            System.out.println("[Auth] Error while validating jwt: " + e);
+            return AuthResponse.INVALID_TOKEN;
+        }
+    }
 
     private static String getSecret() {
         try {
@@ -51,28 +93,7 @@ class Auth {
         }
     }
 
-    public static boolean validate(Object authorizationToken) {
-        try {
-            if (authorizationToken == null) {
-                System.out.println("[Auth] Authorization header not found. Request rejected");
-                return false;
-            }
-
-            String strToken = authorizationToken.toString();
-            if (!strToken.startsWith("bearer ")) {
-                System.out.println("[Auth] Invalid authorization header format. Request rejected");
-                return false;
-            }
-            // remove the "bearer " prefix
-            strToken = strToken.substring(7);
-
-            // HS256 (HMAC with SHA-256): same key for generating and validating.
-            Algorithm algorithm = Algorithm.HMAC256(getSecret());
-            JWT.require(algorithm).build().verify(strToken);
-            return true;
-        } catch (Exception e) {
-            System.out.println("[Auth] Error while validating jwt: " + e);
-            return false;
-        }
+    private static boolean allowedMethod(String method) {
+        return "GET".equals(method) || "HEAD".equals(method) || "OPTIONS".equals(method);
     }
 }
